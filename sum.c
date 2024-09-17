e1d3cd6de21f8da3765dc4f4ce0a52e62774fe78
jaianmol <Anmol20anand@gmail.com>
2024-09-17 20:10:51 +0530

    Create sum.c

---
+++ sum.c
@@ -0,0 +1,14 @@
+
+
+#include <stdio.h>
+#include<conio.h>
+void main()
+{
+ int a,b,add;
+ printf("enter the value of a");
+ printf("enter the value of b");
+ scanf("%d%d",&a,&b);
+ add=a+b;
+ printf("the sum is %d",add);
+getch();
+}
